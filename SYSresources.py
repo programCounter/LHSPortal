ba44247b6d02f284d4ca3a8c4e1f4b8daf693e0d
Riley Larche <riley.larche@gmail.com>
2018-12-23 18:06:06 -0800

    Update SYSresources.py

--- SYSresources.py
+++ SYSresources.py
@@ -47,8 +47,10 @@ def ReadSYS():
     in the script.
     '''
 
-    GetCPU()
-    GetRAM()
-    GetDisks()
+    NUMofCPU = psutil.cpu_count(logical=False) #Return only physical cores (NOT logical)
+    NUMofTHREAD = psutil.cpu_count(logical=True) #Return number of logical threads
+    CPUtotal = psutil.cpu_percent()
+    CPUtotalFreq = psutil.cpu_freq(percpu=True)
+    CPUperCore = psutil.cpu_percent(percpu=True)
 
     return{ 'NUMofCPU':NUMofCPU, 'NUMofTHREAD':NUMofTHREAD, 'CPUtotal':CPUtotal, 'CPUtotalFreq':CPUtotalFreq, 'CPUperCore':CPUperCore, 'VirtualMem':VirtualMem, 'DiskPartitions':DiskPartitions, 'RootDiskUse':RootDiskUse }
