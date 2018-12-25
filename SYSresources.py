b86291a68eb06a35dcbcb8bc4eac10caa5673727
Riley Larche <riley.larche@gmail.com>
2018-12-24 20:08:57 -0800

    Removed unsessassary code

--- SYSresources.py
+++ SYSresources.py
@@ -1,19 +1,6 @@
+#libraries To include
 import psutil
-import time
 
-#CPU Var
-#NUMofCPU = 0
-#NUMofTHREAD = 0
-#CPUtotal = 0.0
-#CPUtotalFreq = []
-#CPUperCore = []
-
-#RAM Var
-#VirtualMem = 0
-
-#Disk Var
-DiskPartitions = 0
-RootDiskUse = 0
 
 def GetCPU():
     NUMofCPU = psutil.cpu_count(logical=False) #Return only physical cores (NOT logical)
@@ -22,29 +9,19 @@ def GetCPU():
     CPUtotalFreq = psutil.cpu_freq(percpu=True)
     CPUperCore = psutil.cpu_percent(percpu=True)
 
-    #print("Number for physical CPU cores: ", NUMofCPU)
-    #print("Number of CPU threads: ", NUMofTHREAD)
-    #print("CPU Total useage: ", CPUtotal)
-    #print("CPU Total Frequency: ", CPUtotalFreq)
-    #print("Per Core CPU useage: ", CPUperCore)
 
 def GetRAM():
     VirtualMem = psutil.virtual_memory()
 
-    #print("RAM: ", VirtualMem)
 
 def GetDisks():
     DiskPartitions = psutil.disk_partitions()
     RootDiskUse = psutil.disk_usage('/') #Provide disk path
 
-    #print("Disk Partitions", DiskPartitions)
-    #print("Root Disk Use", RootDiskUse)
-
 
 def ReadSYS():
     '''
-    This function can be called externally to run all other functions
-    in the script.
+    This function can be called externally to gather system data.
     '''
 
     NUMofCPU = psutil.cpu_count(logical=False) #Return only physical cores (NOT logical)
